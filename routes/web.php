22075a028a90e43e7093fabc94ba11754cf8f6c0
Joel Mnisi <Joel.Mnisi@universal.local>
2018-10-16 13:58:43 +0200

    =Changing welcome to login

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Auth::routes();
-
+Route::redirect('/', '/home');
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/reports', 'HomeController@report')->name('report');
 Route::get('/projects', 'ProjectController@index');
